e3366838ca218e209dc173c008ee5c6c660120a4
Aşkın Özgür <askin@askin.ws>
2019-07-23 23:44:09 +0300

    change indicator 80 to 120, and color

--- elisp/init-fill-column-indicator.el
+++ elisp/init-fill-column-indicator.el
@@ -1,10 +1,15 @@
+;;; init-fill-column-indicator --- Initialize fci-mode
+
+;;; Commentary:
+;;; Code:
+
 (require 'fill-column-indicator)
 
-(setq-default fill-column 80)
+(setq-default fill-column 120)
 
 ;; Fill Column Indicator: https://github.com/alpaker/Fill-Column-Indicator
 (setq fci-rule-width 5)
-(setq fci-rule-color "gray20")
+(setq fci-rule-color "#412E32")
 
 ;; User fci-mode for defined modes
 ;; FIXME: fci-mode doesn't work with modes but python-mode
@@ -14,3 +19,5 @@
  '(python-mode-hook c-mode-hook lisp-mode-hook js-mode-hook python-mode))
 
 (provide 'init-fill-column-indicator)
+
+;;; init-fill-column-indicator.el ends here
