56912b54beb2444952b4342a469972bc6ffde0a9
Aşkın Özgür <askin@askin.ws>
2021-04-25 23:04:51 +0300

    set indent 2 on web mode

--- init.el
+++ init.el
@@ -255,7 +255,9 @@ There are two things you can do about this warning:
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\.twig\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(setq web-mode-markup-indent-offset 4)
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
 
 ;; Syntax Check For All Type Code
 (global-flycheck-mode)
